37a049dcf013d5b65ac3a5a0ce5b3c9b1fac344f
angelsol299 <32056957+angelsol299@users.noreply.github.com>
2018-08-29 22:28:48 +0200

    inittial comit

---
+++ src/components/test/Test.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
+  }
+
+  //   componentWillMount() {
+  //     console.log("component willmount...");
+  //   }
+
+  //   componentDidUpdate() {
+  //     console.log("componentDidUpadate...");
+  //   }
+
+  //   componentWillReceiveProps(nextProps, nextState) {
+  //     console.log("compoentnwill recieveprops");
+  //   }
+
+  //   static getDerivedStateFromProps(nextProps, prevState) {
+  //     return {
+  //       test: "something"
+  //     };
+  //   }
+
+  //   getSnapshotBeforeUpdate(prevProps, prevState) {
+  //     console.log("getsnapshotbeforeupdate");
+  //   }
+
+  render() {
+    const { title, body } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
+
+export default Test;
